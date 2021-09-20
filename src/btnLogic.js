3621a5047d6504b395d1cd257d1ac113538ede89
Matthew <matthewchaplin60@gmail.com>
2021-09-21 03:33:24 +0900

    Added pop up Box

--- src/btnLogic.js
+++ src/btnLogic.js
@@ -2,12 +2,21 @@
 function createNewTDBtn(TDLogic) {
     const addTDBtn = document.getElementById('addTDBtn')
     addTDBtn.addEventListener('click', TDLogic)
+    const popupAdd = document.getElementById('popupAdd')
+    popupAdd.addEventListener('click', openForm);
 }
 
 function deleteTDBtn(item) {
     
 }
 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  } 
 
 
 export {createNewTDBtn}
